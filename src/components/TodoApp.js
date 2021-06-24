b8d054633cb8a390f4622c9c60ceaf7d5125419c
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-06-24 13:00:45 -0500

    Add New todo, implemented

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,10 +1,11 @@
 import React, { useReducer } from 'react';
+import { useForm } from '../hooks/useForm';
 import { todoReducer } from './todoReducer';
 const init = () => {
   return [
     {
       id: new Date().getTime(),
-      description: '',
+      desc: 'Hola',
       done: false,
     },
   ];
@@ -12,29 +13,74 @@ const init = () => {
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, [], init);
 
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: '',
+  });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (description.trim().length <= 1) {
+      return;
+    }
+
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false,
+    };
+
+    const action = {
+      type: 'add',
+      payload: newTodo,
+    };
+
+    dispatch(action);
+    reset();
+  };
+
+  console.log(todos);
+
   return (
     <div>
       <h1 className="text-5xl pb-4 border-b-2 border-gray-300">
         TODO Aplication
       </h1>
-      <h2 className="text-3xl mt-2">Number of TODOs</h2>
+      <h2 className="text-3xl mt-2">Number of TODOs {todos.length}</h2>
       <div className="flex">
         <div className="w-1/2">
           <ul>
-            <p></p>
+            {todos.map((todo, i) => (
+              <li key={todo.id}>
+                <p>
+                  {i + 1}: {todo.desc}
+                </p>
+              </li>
+            ))}
           </ul>
         </div>
+
         <div className="w-1/3 border-2 border-gray-500 p-5">
           <h1 className="pb-2 border-b-2 border-gray-300 text-4xl mb-3">
             Add TODO
           </h1>
-          <input
-            placeholder="Add New TODO"
-            className="mt-5 w-full h-10 border-2 border-gray-700 rounded text-center "
-          />
-          <button className="w-full mt-5 px-6 py-3 rounded font-medium bg-blue-500 text-white hover:bg-white hover:text-blue-500 border-2 border-blue-500 transition duration-250	">
-            Add
-          </button>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Add New TODO"
+              name="description"
+              autoComplete="off"
+              className="mt-5 w-full h-10 border-2 border-gray-700 rounded text-center "
+              onChange={handleInputChange}
+              value={description}
+            />
+            <button
+              type="submit"
+              className="w-full mt-5 px-6 py-3 rounded font-medium bg-blue-500 text-white hover:bg-white hover:text-blue-500 border-2 border-blue-500 transition duration-250	"
+            >
+              Add
+            </button>
+          </form>
         </div>
       </div>
     </div>
--- src/components/todoReducer.js
+++ src/components/todoReducer.js
@@ -1,10 +1,9 @@
 export const todoReducer = (state, action) => {
-  switch (action) {
-    // case value:
-
-    //     break;
+  switch (action.type) {
+    case 'add':
+      return [...state, action.payload];
 
     default:
-      break;
+      return state;
   }
 };
